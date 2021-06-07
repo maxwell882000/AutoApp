c672f1da1a93a0f5673ccbc3cce2ac3af5d22438
Shokhzod <you@example.com>
2021-06-07 12:00:47 +0500

    SAD

--- paynet_web_server/routing.py
+++ paynet_web_server/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/paynet_pay/$', consumers.PaynetServer.as_asgi()),
+    re_path(r'ws/paynet/server/$', consumers.PaynetServer.as_asgi()),
 ]
