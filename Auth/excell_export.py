01e5f4166f6c4b25bcdcba4ac90f69449150453c
Shokhzod <you@example.com>
2021-06-15 15:49:00 +0500

    AA

--- Auth/excell_export.py
+++ Auth/excell_export.py
@@ -2,6 +2,33 @@ from import_export import resources
 from Auth.models import UserTransport
 
 
+def card(name: str):
+    relation = "cards__" + name
+    return relation
+
+
+def card_user(name: str):
+    return card("cards_user__card__" + name)
+
+
+def cards_user_expenses(name: str):
+    return card_user("expense__" + name)
+
+
+def cards_all():
+    return (card('model'), card('marka'),
+            card('yearOfMade'), card('number'),
+            card('yearOfPurchase'), card('numberOfTank'),
+            card('firstTankType'), card('firstTankVolume'),
+            card('secondTankType'), card('secondTankVolume'),
+            card('run'), card('initial_run'), card('tech_passport'),
+            card_user("name_of_card"), card_user('date'), card_user('comments'),
+            cards_user_expenses('name'), cards_user_expenses('sum'),
+            cards_user_expenses('amount')
+            )
+
+
 class UserResource(resources.ModelResource):
     class Meta:
         model = UserTransport
+        fields = cards_all()
