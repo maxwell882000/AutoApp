198bd345abf9f6ad724338117bd93c6cd8d0838e
Shokhzod <you@example.com>
2021-06-17 11:00:43 +0500

    ASD

--- AutoApp/settings.py
+++ AutoApp/settings.py
@@ -131,9 +131,17 @@ WSGI_APPLICATION = 'AutoApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'autoapp',
+        'USER': 'vid',
+        'PASSWORD': '123123_',
+        'HOST': 'localhost',
+        'PORT': '',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 # Password validation
