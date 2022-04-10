e3e9311c629a0080aefaddfc8a4e5a6b1a521b7c
Shokhzod Murodov <m_axwel_l@Shokhzods-MacBook-Pro.local>
2022-04-10 12:46:43 +0500

    adding allowed host

--- Auth/urls.py
+++ Auth/urls.py
@@ -15,7 +15,6 @@ api = Application(services=[pp], tns='http://uws.provider.com/', name="ProviderW
                   in_protocol=Soap11(validator='soft'), out_protocol=Soap11(), )
 urlpatterns = [
                   path('login/', RegisterOrLoginUsersViews.as_view()),
-
                   path('register/', AccountRegister.as_view()),
                   path('transport/', TransportViews.as_view()),
                   path('transport/<pk>/', TransportViews.as_view()),
